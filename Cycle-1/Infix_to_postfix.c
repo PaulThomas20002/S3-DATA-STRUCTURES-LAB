ea9b8e7a0e9d053db531d3c2724c494704608932
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2022-01-02 19:04:55 +0530

    Update Infix_to_postfix.c

--- Cycle-1/Infix_to_postfix.c
+++ Cycle-1/Infix_to_postfix.c
@@ -1,5 +1,5 @@
 /*
-Algorithm 
+Algorithm to convert Infix to postfix expression
 1. Scan the infix expression from left to right. 
 2. If the scanned character is an operand, output it. 
 3. Else, 
@@ -10,4 +10,11 @@ Algorithm
 6. Repeat steps 2-6 until infix expression is scanned. 
 7. Print the output 
 8. Pop and output from the stack until it is not empty.
+
+Algorithm for evaluation postfix expressions. 
+1) Create a stack to store operands (or values). 
+2) Scan the given expression and do the following for every scanned element. 
+…..a) If the element is a number, push it into the stack 
+…..b) If the element is an operator, pop 2 operands for the operator from the stack. Evaluate the operator and push the result back to the stack 
+3) When the expression is ended, the number in the stack is the final answer
 */
