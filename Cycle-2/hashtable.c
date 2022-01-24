722462dbec0b39c04fc415dc7867059f45ac3772
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2022-01-24 09:14:27 +0530

    Update hashtable.c

--- Cycle-2/hashtable.c
+++ Cycle-2/hashtable.c
@@ -1,86 +1,88 @@
 #include <stdio.h>
 #include <math.h>
-#define HASH_TABLE_SIZE 10
-int hash_table[HASH_TABLE_SIZE]={0};
+#define SIZE 10
+
+int HT[SIZE]={0};
+
 int hash(int data){
     int sum=0;
-    while(data!=0){
-        sum+=pow(data%10,2);
+    while(data != 0){
+        sum += pow(data%10,2);
         data/=10;
-        }
-return sum%HASH_TABLE_SIZE;
-    
+    }
+return sum%SIZE;
 }
-int insert_ht(int ele){
-    int index=hash(ele);
-    if( hash_table[index]==0 ){
-        hash_table[index]=ele;
+
+int insert(int item){
+    int index=hash(item);
+    if( HT[index]==0 ){
+        HT[index]=item;
         return 1;
+    }
+    //linear probing 
+    int new_index=(index+1)%SIZE;
+    while(new_index!=index){
+        if(HT[new_index]==0){
+            HT[new_index]=item;
+            return 1;
         }
-        //linear probing 
-        int new_index=(index+1)%HASH_TABLE_SIZE;
-while(new_index!=index){
-    if(hash_table[new_index]==0){
-        hash_table[new_index]=ele;
-        return 1;
-        }
-        new_index=(new_index+1)%HASH_TABLE_SIZE;
-        }
-        return 0;
-    
+    new_index=(new_index+1)%SIZE;
+    }
+return 0;
 }
-int delete_ht(int ele){
-    int index=hash(ele);
-if(hash_table[index]==ele){
-    hash_table[index]=0;
-    return 1;
+
+int del(int item){
+    int index=hash(item);
+    if(HT[index]==item){
+        HT[index]=0;
+        return 1;
     }
     //linear probing
-    
-    int new_index=(index+1)%HASH_TABLE_SIZE;
- while(new_index!=index){
-     if(hash_table[new_index]==ele){
-         hash_table[new_index]=0;
-         return 1;
+    int new_index=(index+1)%SIZE;
+    while(new_index!=index){
+        if(HT[new_index]==item){
+            HT[new_index]=0;
+            return 1;
          }
-         new_index=(new_index+1)%HASH_TABLE_SIZE;
-         }
-         return 0;}
-void display_ht(){
+    new_index=(new_index+1)%SIZE;
+    }
+    return 0;
+}
+
+void display(){
     printf("\n***HASH TABLE***\n\n");
-    for(int i=0;i<HASH_TABLE_SIZE;i++){
+    for(int i=0;i<SIZE;i++){
         printf("\t%d : ",i);
-        if(hash_table[i] !=0)
-        printf("%d",hash_table[i]);
+        if(HT[i] !=0)
+            printf("%d",HT[i]);
         printf("\n");
         }
-        printf("\n***HASH TABLE***\n\n");}
+        printf("\n***HASH TABLE***\n\n");
+}
+
 int main(){
-    int option;
-    int value;
+    int d, value;
     do{
-        printf("1.insert\n2.delete\n3.display\n4.exit\nchoose any option : ");
-        scanf("%d",&option);
-        switch(option){
-            case 1:
-            printf("Enter the key : ");
-            scanf("%d",&value);
-            if( insert_ht(value)==0 )
-            printf("\nTable Full\n");
-            break;
-            case 2:
-            printf("Enter the key : ");
-            scanf("%d",&value);
-            if( delete_ht(value)==0 )
-            printf("\nElement not found\n");
-            break;
-            case 3:
-            display_ht();
-            break;
-            default:
-            printf("\nEnter a valid option\n");
-            }
-}while(option!=4);
+        printf("1.Insert\n2.Delete\n3.Display\n4.Exit\nchoose any Option : ");
+        scanf("%d",&d);
+        switch(d){
+            case 1 : printf("Enter the key : ");
+                     scanf("%d",&value);
+                     if( insert(value)==0 )
+                        printf("\nTable Full\n");
+                     break;
+            case 2 : printf("Enter the key : ");
+                     scanf("%d",&value);
+                     if( del(value)==0 )
+                        printf("\nElement not found\n");
+                     break;
+            case 3 : display();
+                     break;
+            default :printf("\nTRY AGAIN!\n");
+                     break;
+            
+        }
+}while(d!=4);
 return 0;
     
 }
