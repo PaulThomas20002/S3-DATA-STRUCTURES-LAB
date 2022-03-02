9b3ca590de12362e049228c62c469312b415adf5
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2022-03-03 00:11:32 +0530

    Update bfs3.c

--- Cycle-3/bfs3.c
+++ Cycle-3/bfs3.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int q[20],top=-1,front=-1,rear=-1,a[20][20],vis[20],stack[20];
+int q[20], top = -1, front = -1, rear = -1, a[20][20], vis[20], stack[20];
 int delete();
 void add(int item);
 void bfs(int s,int n);
@@ -8,40 +8,36 @@ void bfs(int s,int n);
 void main(){
     int n,i,s,ch,j;
     char c;
-    printf("ENTER THE NUMBER VERTICES ");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        for(j=1;j<=n;j++){
-            printf("ENTER 1 IF %d HAS A NODE WITH %d ELSE 0 ",i,j);
-            scanf("%d",&a[i][j]);
+    printf("Enter the number vertices : ");
+    scanf("%d", &n);
+    for(i = 1;i <= n;i++){
+        for(j = 1;j <= n;j++){
+            printf("Enter 1 if %d has a node with %d else 0 ",i,j);
+            scanf("%d", &a[i][j]);
         }
     }
-    printf("THE ADJACENCY MATRIX IS\n");
-    for(i=1;i<=n;i++){
-        for(j=1;j<=n;j++){
+    printf("The Adjacency Matrix\n");
+    for(i = 1;i <= n;i++){
+        for(j = 1;j <= n;j++){
             printf(" %d ",a[i][j]);
         }
     printf("\n");
     }
 
     do{
-        for(i=1;i<=n;i++)
-            vis[i]=0;
-        printf("\nMENU");
-        printf("\n1.B.F.S");
-        printf("\n0 to Exit");
-        printf("\nENTER YOUR CHOICE");
+        for(i = 1;i <= n;i++)
+            vis[i] = 0;
+
+        printf("\nMenu\n1.B.F.S\n0 to Exit\nEnter the option : ");
         scanf("%d",&ch);
-        printf("ENTER THE SOURCE VERTEX :");
+        printf("Enter the source vertex :");
         scanf("%d",&s);
 
         switch(ch){
-        case 1:
-            bfs(s,n);
-            break;
-        case 0:
-            printf("Exiting...");
-            break;
+        case 1 : bfs(s,n);
+                 break;
+        case 0 : printf("Exiting.");
+                 break;
         }
     }while(ch!=0);
 }
@@ -49,45 +45,45 @@ void main(){
 void bfs(int s,int n){
     int p,i;
     add(s);
-    vis[s]=1;
-    p=delete();
-    if(p!=0)
+    vis[s] = 1;
+    p = delete();
+    if(p != 0)
         printf(" %d",p);
-    while(p!=0){
-        for(i=1;i<=n;i++)
-            if((a[p][i]!=0)&&(vis[i]==0)){
-            add(i);
-            vis[i]=1;
+    while(p != 0){
+        for(i = 1;i <= n;i++)
+            if((a[p][i] != 0) && (vis[i] == 0)){
+                add(i);
+                vis[i] = 1;
             }
-        p=delete();
-        if(p!=0)
+        p = delete();
+        if(p != 0)
             printf(" %d ",p);
     }
-    for(i=1;i<=n;i++)
-        if(vis[i]==0)
+    for(i = 1;i <= n;i++)
+        if(vis[i] == 0)
             bfs(i,n);
 }
 
 
 void add(int item){
-    if(rear==19)
-        printf("QUEUE FULL");
+    if(rear == 19)
+        printf("Queue full.");
     else{
-        if(rear==-1){
-            q[++rear]=item;
+        if(rear == -1){
+            q[++rear] = item;
             front++;
         }
         else
-            q[++rear]=item;
+            q[++rear] = item;
     }
 }
 
 int delete(){
     int k;
-    if((front>rear)||(front==-1))
+    if((front > rear) || (front == -1))
         return(0);
     else{
-        k=q[front++];
+        k = q[front++];
         return(k);
     }
 }
